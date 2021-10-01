864525b42fd1d00ac47f0f8dea0abd52d03f8fad
Jesus <Jvillalba23@yahoo.com>
2021-09-30 19:57:06 -0500

    model

--- watsoninvestment/src/main/java/com/Jesus/watsoninvestment/models/Crypto.java
+++ watsoninvestment/src/main/java/com/Jesus/watsoninvestment/models/Crypto.java
@@ -1,5 +1,131 @@
 package com.Jesus.watsoninvestment.models;
 
-public class Crypto {
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="cryptos")
+public class Crypto {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@DateTimeFormat(pattern ="yyyy-MM-dd, HH:mm")
+	private Date cryptoDate;
+	
+	@Column(updatable=false)
+	private Date createdAt;
+	private Date updatedAt;
+	
+	@NotNull
+	private int price;
+	
+	@NotBlank
+	private String learn;
+	
+	@NotBlank
+	private String individual;
+	
+	@NotBlank
+	private String business;
+	
+	@NotBlank
+	private String developer;
+	
+	@NotBlank
+	private String company;
+
+	public Crypto() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getCryptoDate() {
+		return cryptoDate;
+	}
+
+	public void setCryptoDate(Date cryptoDate) {
+		this.cryptoDate = cryptoDate;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getLearn() {
+		return learn;
+	}
+
+	public void setLearn(String learn) {
+		this.learn = learn;
+	}
+
+	public String getIndividual() {
+		return individual;
+	}
+
+	public void setIndividual(String individual) {
+		this.individual = individual;
+	}
+
+	public String getBusiness() {
+		return business;
+	}
+
+	public void setBusiness(String business) {
+		this.business = business;
+	}
+
+	public String getDeveloper() {
+		return developer;
+	}
+
+	public void setDeveloper(String developer) {
+		this.developer = developer;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
 }
---
+++ watsoninvestment/src/main/java/com/Jesus/watsoninvestment/repositories/CryptoRepository.java
@@ -0,0 +1,10 @@
+package com.Jesus.watsoninvestment.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CryptoRepository extends CrudRepository {
+	
+
+}
